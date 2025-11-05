a5a2f5cc3cebd77084e0f6ee93aa664531f911e7
yaseen7069 <ysnmhd786@gmail.com>
2025-11-05 15:45:41 +0530

    Add files via upload

---
+++ calcmain.py
@@ -0,0 +1,7 @@
+from mathoperations.calcfunctions import*
+x=int (input("enter first number"))
+y=int (input("enter second number"))
+print (addition(x,y))
+print (subtraction(x,y))
+print (multiplication(x,y))
+print (division(x,y))
